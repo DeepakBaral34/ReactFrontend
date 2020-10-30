750968abf814d76cc65cdb46d312f9f09d7ebd45
DeepakBaral34 <51141903+DeepakBaral34@users.noreply.github.com>
2020-10-30 12:25:04 +0545

    final commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,97 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const buttonstyle = {
+  padding: "10px",
+  width: "300px"
+};
+var buttonlink = "./mainpage.html";
+function info1() {
+return (document.getElementById("displayIframe").src="./page1.html");
+}
+
+function info2() {
+return (document.getElementById("displayIframe").src="./page2.html");
+}
+
+function info3() {
+return (document.getElementById("displayIframe").src="./page3.html");
+}
+
+function info4() {
+return (document.getElementById("displayIframe").src="./page4.html");
+}
+
+function info5() {
+return (document.getElementById("displayIframe").src="./page5.html");
+}
+
+function info6() {
+return (document.getElementById("displayIframe").src="./page6.html");
+}
+
+function info7() {
+return (document.getElementById("displayIframe").src="./page7.html");
+}
+
+function info8() {
+return (document.getElementById("displayIframe").src="./page8.html");
+}
+
+function info9() {
+return (document.getElementById("displayIframe").src="./page9.html");
+}
+
+function info10() {
+return (document.getElementById("displayIframe").src="./page10.html");
+}
+
+function info11() {
+return (document.getElementById("displayIframe").src="./page11.html");
+}
+
+const custompage = (
+  <div>
+  <div>
+    <div>
+      <button style={buttonstyle} onClick={info1}> User Management </button>
+    </div>
+    <div>
+      <button style={buttonstyle} onClick={info2}> Company information/Profile </button>
+    </div>
+    <div>
+      <button style={buttonstyle} onClick={info3}> Test Groups Management </button>
+    </div>
+    <div>
+      <button style={buttonstyle} onClick={info4}> Test Management </button>
+    </div>
+    <div>
+      <button style={buttonstyle} onClick={info5}> Doctors Management </button>
+    </div>
+    <div>
+      <button style={buttonstyle} onClick={info6}> Lab Technicians </button>
+    </div>
+    <div>
+      <button style={buttonstyle} onClick={info7}> Patient entry </button>
+    </div>
+    <div>
+      <button style={buttonstyle} onClick={info8}> Sample Collection </button>
+    </div>
+    <div>
+      <button style={buttonstyle} onClick={info9}> Result Entry </button>
+    </div>
+    <div>
+      <button style={buttonstyle} onClick={info10}> Billing History </button>
+    </div>
+    <div>
+      <button style={buttonstyle} onClick={info11}> Test Analysis </button>
+    </div>
+    </div>
+    <div style={{position:"absolute", top: "0px", left: "350px"}}>
+    <iframe id="displayIframe" src={buttonlink} style={{height:"500px", width:"900px"}}> </iframe>
+    </div>
+    </div>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(custompage, document.getElementById('root'));
